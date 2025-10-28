160dd59ad7ed6d9492727cce9e6731bba3377880
moro-qq82 <59406746+moro-qq82@users.noreply.github.com>
2025-10-29 08:16:59 +0900

    Implement training data module and trainer

--- src/train/__init__.py
+++ src/train/__init__.py
@@ -1,5 +1,17 @@
 """Training utilities for ARC mutual concept loss."""
 
+from .data_module import ARCDataModule, ARCDataModuleConfig, ARCProcessedTaskDataset
 from .loss_factory import LossFactory, LossFactoryConfig
+from .trainer import ARCTrainer, OptimizerConfig, SchedulerConfig, TrainerConfig
 
-__all__ = ["LossFactory", "LossFactoryConfig"]
+__all__ = [
+    "ARCDataModule",
+    "ARCDataModuleConfig",
+    "ARCProcessedTaskDataset",
+    "LossFactory",
+    "LossFactoryConfig",
+    "ARCTrainer",
+    "OptimizerConfig",
+    "SchedulerConfig",
+    "TrainerConfig",
+]
